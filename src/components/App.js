981ca7351824b31d6080ad7a11cbd66c4f36963b
Subject6735 <se4n01@gmail.com>
2022-06-03 22:03:10 +0200

    overlay, footer function instead of class, removed strictmode

--- src/components/App.js
+++ src/components/App.js
@@ -82,7 +82,12 @@ class App extends React.Component {
 
         await this.timeout(3000).then(() => {
             this.setState((state) => ({
-                score: (player === 'Rock' && house === 'Scissors') || (player === 'Paper' && house === 'Rock') || (player === 'Scissors' && house === 'Paper') ? state.score + 1 : state.score,
+                score:
+                    (player === 'Rock' && house === 'Scissors') ||
+                    (player === 'Paper' && house === 'Rock') ||
+                    (player === 'Scissors' && house === 'Paper')
+                        ? state.score + 1
+                        : state.score,
                 replayDisplayed: true,
             }));
         });
@@ -100,105 +105,99 @@ class App extends React.Component {
         }));
     };
 
-    rulesClick = () => {
-        this.setState((state) => ({
-            rulesDisplayed: true,
-        }));
-        document.querySelector('#root').classList.add('overlay');
-    };
-
-    closeClick = () => {
-        this.setState((state) => ({
-            rulesDisplayed: false,
-        }));
-        document.querySelector('#root').classList.remove('overlay');
-    };
-
     render() {
-        return (
-            <React.StrictMode>
-                <Header score={this.state.score} />
-                <main className="grid place-content-center">
-                    <div className={`${this.state.gameStarted ? 'hidden' : 'grid'} choices grid-rows-2 grid-cols-2 gap-3 place-content-center`}>
-                        <Rock classList={'row-start-1 row-end-1 col-start-1 col-end-1'} clickable={true} onClick={(e) => this.startGame(e)} />
-                        <Paper classList={'row-start-1 row-end-1 col-start-2 col-end-2'} clickable={true} onClick={(e) => this.startGame(e)} />
-                        <Scissors classList={'row-start-2 row-end-2 col-span-2'} clickable={true} onClick={(e) => this.startGame(e)} />
-                    </div>
+        return [
+            <Header key="header" score={this.state.score} />,
+            <main key="main" className="grid place-content-center">
+                <div className={`${this.state.gameStarted ? 'hidden' : 'grid'} choices grid-rows-2 grid-cols-2 gap-3 place-content-center`}>
+                    <Rock classList={'row-start-1 row-end-1 col-start-1 col-end-1'} clickable={true} onClick={(e) => this.startGame(e)} />
+                    <Paper classList={'row-start-1 row-end-1 col-start-2 col-end-2'} clickable={true} onClick={(e) => this.startGame(e)} />
+                    <Scissors classList={'row-start-2 row-end-2 col-span-2'} clickable={true} onClick={(e) => this.startGame(e)} />
+                </div>
 
-                    <div className={`${this.state.counterEnded ? 'hidden' : 'grid'} place-content-center uppercase tracking-widest text-lg`}>
-                        {this.state.counterText === 'Rock' ? (
+                <div className={`${this.state.counterEnded ? 'hidden' : 'grid'} place-content-center uppercase tracking-widest text-lg`}>
+                    {this.state.counterText === 'Rock' ? (
+                        <Rock clickable={false} />
+                    ) : this.state.counterText === 'Paper' ? (
+                        <Paper clickable={false} />
+                    ) : this.state.counterText === 'Scissors' ? (
+                        <Scissors clickable={false} />
+                    ) : (
+                        ''
+                    )}
+                </div>
+
+                <div className={`${this.state.resultDisplayed ? 'grid' : 'hidden'} gap-3`}>
+                    <div
+                        className={`${
+                            !this.state.playerDisplayed ? 'row-span-2' : 'self-center'
+                        } uppercase tracking-widest text-lg row-start-1 row-end-1 col-start-1 col-end-1 justify-self-center`}
+                    >
+                        You Picked
+                    </div>
+                    <div
+                        className={`${
+                            this.state.playerDisplayed ? 'block' : 'hidden'
+                        } animation-fade-in row-start-2 row-end-2 col-start-1 col-end-1 place-self-center`}
+                    >
+                        {this.state.player === 'Rock' ? (
                             <Rock clickable={false} />
-                        ) : this.state.counterText === 'Paper' ? (
+                        ) : this.state.player === 'Paper' ? (
                             <Paper clickable={false} />
-                        ) : this.state.counterText === 'Scissors' ? (
+                        ) : this.state.player === 'Scissors' ? (
                             <Scissors clickable={false} />
-                        ) : (
-                            ''
-                        )}
+                        ) : null}
                     </div>
 
-                    <div className={`${this.state.resultDisplayed ? 'grid' : 'hidden'} gap-3`}>
-                        <div
-                            className={`${
-                                !this.state.playerDisplayed ? 'row-span-2' : 'self-center'
-                            } uppercase tracking-widest text-lg row-start-1 row-end-1 col-start-1 col-end-1 justify-self-center`}
-                        >
-                            You Picked
-                        </div>
-                        <div className={`${this.state.playerDisplayed ? 'block' : 'hidden'} animation-fade-in row-start-2 row-end-2 col-start-1 col-end-1 place-self-center`}>
-                            {this.state.player === 'Rock' ? (
-                                <Rock clickable={false} />
-                            ) : this.state.player === 'Paper' ? (
-                                <Paper clickable={false} />
-                            ) : this.state.player === 'Scissors' ? (
-                                <Scissors clickable={false} />
-                            ) : null}
-                        </div>
-
-                        <div
-                            className={`${
-                                this.state.replayDisplayed ? 'block' : 'hidden'
-                            } animation-fade-in col-start-2 col-end-2 row-start-2 row-end-2 row-span-2 justify-self-center self-center text-center`}
-                        >
-                            <div className="uppercase text-4xl mb-5">
-                                {this.state.player === 'Rock' && this.state.house === 'Paper'
-                                    ? 'You Lose'
-                                    : this.state.player === 'Paper' && this.state.house === 'Scissors'
-                                    ? 'You Lose'
-                                    : this.state.player === 'Scissors' && this.state.house === 'Rock'
-                                    ? 'You Lose'
-                                    : this.state.player === this.state.house
-                                    ? 'Draw'
-                                    : 'You Win'}
-                            </div>
-
-                            <div
-                                className="bg-white text-textDark hover:text-red-500 uppercase tracking-widest text-lg rounded-md pt-5 pb-5 pl-12 pr-12 h-10 grid place-content-center cursor-pointer"
-                                onClick={() => this.newGame()}
-                            >
-                                Play again
-                            </div>
+                    <div
+                        className={`${
+                            this.state.replayDisplayed ? 'block' : 'hidden'
+                        } animation-fade-in col-start-2 col-end-2 row-start-2 row-end-2 row-span-2 justify-self-center self-center text-center`}
+                    >
+                        <div className="uppercase text-4xl mb-5">
+                            {this.state.player === 'Rock' && this.state.house === 'Paper'
+                                ? 'You Lose'
+                                : this.state.player === 'Paper' && this.state.house === 'Scissors'
+                                ? 'You Lose'
+                                : this.state.player === 'Scissors' && this.state.house === 'Rock'
+                                ? 'You Lose'
+                                : this.state.player === this.state.house
+                                ? 'Draw'
+                                : 'You Win'}
                         </div>
 
                         <div
-                            className={`${!this.state.houseDisplayed ? 'row-span-2' : 'self-center'} uppercase tracking-widest text-lg row-start-1 row-end-1 col-start-3 col-end-3 justify-self-center`}
+                            className="bg-white text-textDark hover:text-red-500 uppercase tracking-widest text-lg rounded-md pt-5 pb-5 pl-12 pr-12 h-10 grid place-content-center cursor-pointer"
+                            onClick={() => this.newGame()}
                         >
-                            The House Picked
-                        </div>
-                        <div className={`${this.state.houseDisplayed ? 'block' : 'hidden'} animation-fade-in row-start-2 row-end-2 col-start-3 col-end-3 place-self-center`}>
-                            {this.state.house === 'Rock' ? (
-                                <Rock clickable={false} />
-                            ) : this.state.house === 'Paper' ? (
-                                <Paper clickable={false} />
-                            ) : this.state.house === 'Scissors' ? (
-                                <Scissors clickable={false} />
-                            ) : null}
+                            Play again
                         </div>
                     </div>
-                </main>
-                <Footer />
-            </React.StrictMode>
-        );
+
+                    <div
+                        className={`${
+                            !this.state.houseDisplayed ? 'row-span-2' : 'self-center'
+                        } uppercase tracking-widest text-lg row-start-1 row-end-1 col-start-3 col-end-3 justify-self-center`}
+                    >
+                        The House Picked
+                    </div>
+                    <div
+                        className={`${
+                            this.state.houseDisplayed ? 'block' : 'hidden'
+                        } animation-fade-in row-start-2 row-end-2 col-start-3 col-end-3 place-self-center`}
+                    >
+                        {this.state.house === 'Rock' ? (
+                            <Rock clickable={false} />
+                        ) : this.state.house === 'Paper' ? (
+                            <Paper clickable={false} />
+                        ) : this.state.house === 'Scissors' ? (
+                            <Scissors clickable={false} />
+                        ) : null}
+                    </div>
+                </div>
+            </main>,
+            <Footer key="footer" />,
+        ];
     }
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,88 +1,59 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import rulesImg from '../images/image-rules.svg';
 import closeIcon from '../images/icon-close.svg';
 import '../index.css';
-/*
+
 function Footer() {
+    const [rulesDisplayed, toggleRules] = useState(false);
+
+    useEffect(() => {
+        const overlay = document.querySelector('div.overlay');
+        rulesDisplayed ? (overlay.style.display = 'block') : (overlay.style.display = 'none');
+    });
+
     return (
-        <footer className="mb-5 justify-self-center self-end text-center">
-            Challenge by{' '}
-            <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
-                Frontend Mentor
-            </a>
-            . Coded by{' '}
-            <a href="https://www.frontendmentor.io/profile/Subject6735" target="_blank" rel="noreferrer">
-                Deme Maráki
-            </a>
-            .
+        <footer className="grid grid-cols-1 gap-3 place-content-center self-end justify-self-center w-full mb-5 text-center">
+            <button
+                onClick={() => toggleRules(true)}
+                className={`${
+                    rulesDisplayed ? 'cursor-not-allowed' : 'cursor-pointer'
+                } border-solid border-2 border-white hover:bg-white hover:text-textDark uppercase pt-2 pb-2 pl-10 pr-10 rounded-lg tracking-widest self-center justify-self-center w-auto md:justify-self-end md:mr-5`} // absolute bottom-5 right-5
+            >
+                Rules
+            </button>
+
+            <div
+                className={`${
+                    rulesDisplayed ? 'grid' : 'hidden'
+                } bg-white rounded-md p-8 grid-cols-1 z-30 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full md:grid-cols-2 md:w-auto md:h-auto`}
+            >
+                <div className="text-textDark font-bold place-self-center text-3xl uppercase md:row-start-1 md:row-end-1 md:col-start-1 md:col-end-1 md:justify-self-start">
+                    Rules
+                </div>
+                <img className="place-self-center col-span-2 pt-10 md:row-start-2 md:row-end-2" src={rulesImg} alt="rules" />
+                <img
+                    onClick={() => toggleRules(false)}
+                    className="place-self-center cursor-pointer md:row-start-1 md:row-end-1 md:col-start-2 md:col-end-2 md:justify-self-end"
+                    src={closeIcon}
+                    alt="close"
+                />
+            </div>
         </footer>
     );
 }
-*/
-
-class Footer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            rulesDisplayed: false,
-        };
-    }
-
-    rulesClick = () => {
-        this.setState((state) => ({
-            rulesDisplayed: true,
-        }));
-    };
-
-    closeClick = () => {
-        this.setState((state) => ({
-            rulesDisplayed: false,
-        }));
-    };
-
-    render() {
-        return (
-            <footer className="grid grid-rows-2 grid-cols-1 gap-3 place-content-center self-end justify-self-center w-auto mb-10 text-center">
-                <button
-                    onClick={() => this.rulesClick()}
-                    className={`${
-                        this.state.rulesDisplayed ? 'cursor-not-allowed' : 'cursor-pointer'
-                    } border-solid border-2 border-white hover:bg-white hover:text-textDark uppercase pt-2 pb-2 pl-10 pr-10 rounded-lg tracking-widest justify-self-center self-center w-1/2`} // absolute bottom-5 right-5
-                    disabled={this.state.rulesDisplayed}
-                >
-                    Rules
-                </button>
-
-                <div
-                    className={`${
-                        this.state.rulesDisplayed ? 'grid' : 'hidden'
-                    } bg-white rounded-md p-8 grid-cols-1 z-3 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full md:grid-cols-2 md:w-auto md:h-auto`}
-                >
-                    <div className="text-textDark font-bold place-self-center text-3xl uppercase md:row-start-1 md:row-end-1 md:col-start-1 md:col-end-1 md:justify-self-start">
-                        Rules
-                    </div>
-                    <img className="place-self-center col-span-2 pt-10 md:row-start-2 md:row-end-2" src={rulesImg} alt="rules" />
-                    <img
-                        onClick={() => this.closeClick()}
-                        className="place-self-center cursor-pointer md:row-start-1 md:row-end-1 md:col-start-2 md:col-end-2 md:justify-self-end"
-                        src={closeIcon}
-                        alt="close"
-                    />
-                </div>
-                <div>
-                    Challenge by{' '}
-                    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
-                        Frontend Mentor
-                    </a>
-                    . Coded by{' '}
-                    <a href="https://www.frontendmentor.io/profile/Subject6735" target="_blank" rel="noreferrer">
-                        Deme Maráki
-                    </a>
-                    .
-                </div>
-            </footer>
-        );
-    }
-}
 
 export default Footer;
+
+/*
+<div className="mt-5 italic">
+    Challenge by{' '}
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
+        Frontend Mentor
+    </a>
+    . Coded by{' '}
+    <a href="https://www.frontendmentor.io/profile/Subject6735" target="_blank" rel="noreferrer">
+        Deme Maráki
+    </a>
+    .
+</div>
+*/
