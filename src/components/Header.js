ea87602ce1a423cdc2305a473f82ed8f89c50e0a
Subject6735 <se4n01@gmail.com>
2022-06-13 14:13:54 +0200

    header css without tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from '../images/logo-bonus.svg';
-import '../index.css';
+import img_logo from '../images/logo-bonus.svg';
+import '../style/header.css';
 
 function Header(props) {
     return (
-        <header className="grid grid-rows-1 grid-cols-3 place-content-center gap-3 rounded-md border-2 border-solid border-white p-4 m-4 mt-10 self-start justify-self-center">
-            <img className="place-self-center" src={logo} alt="logo" />
-            <div className="grid place-content-center col-start-3 place-self-center w-full h-full rounded-md bg-white text-textDark">
-                <div className="place-self-center uppercase text-xl tracking-widest">Score</div>
-                <div className="place-self-center text-6xl">{props.score}</div>
+        <header>
+            <img src={img_logo} alt="logo" />
+            <div className="score">
+                <div>Score</div>
+                <div>{props.score}</div>
             </div>
         </header>
     );
